af111bd86e09611a8cf057d0472bbef217bcec37
CocoCSforever <yijia.ma18@gmail.com>
2025-03-15 15:30:04 -0700

    Updated with llm integration

--- backend/model_2/model.py
+++ backend/model_2/model.py
@@ -1,9 +1,10 @@
 """Sample answers from LLMs on QA task."""
 import logging
-from utils import utils
+# from utils import utils
 import atexit
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from src import chatbot
 
 
 app = Flask(__name__)
@@ -13,33 +14,33 @@ app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 
 # set up
-utils.setup_logger()
-parser = utils.get_parser()
-args, unknown = parser.parse_known_args()
-logging.info('Starting new run with args: %s', args)
+# utils.setup_logger()
+# parser = utils.get_parser()
+# args, unknown = parser.parse_known_args()
+# logging.info('Starting new run with args: %s', args)
 
 # if unknown:
 #     raise ValueError(f'Unkown args: {unknown}')
 
 # Initialize model.
-model = utils.init_model(args)
+# model = utils.init_model(args)
+chatAgent = chatbot.Chatbot()
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    # print("Request received!!!!!!!!!!!!!!!!")
-
+    print("Request received!!!!!!!!!!!!!!!!")
     try:
         user_input = request.json.get('message')
         if not user_input:
             return jsonify({'error': 'No message provided'}), 400
-        local_prompt = f"You are Nova, a wise tarot oracle. You specialize in providing insightful tarot readings and thoughtful guidance." \
-        f"Based on the ${user_input}, greet the user and give a response for tarrot reading." \
-        f"Do not ask more questions or personal information." 
+        # local_prompt = f"You are Nova, a wise tarot oracle. You specialize in providing insightful tarot readings and thoughtful guidance." \
+        # f"Based on the ${user_input}, greet the user and give a response for tarrot reading." \
+        # f"Do not ask more questions or personal information." 
 
-        predicted_answer, token_log_likelihoods, embedding = model.predict(
-            local_prompt, args.temperature)
-        embedding = embedding.cpu() if embedding is not None else None
-
+        # predicted_answer, token_log_likelihoods, embedding = model.predict(
+        #     local_prompt, args.temperature)
+        # embedding = embedding.cpu() if embedding is not None else None
+        predicted_answer = chatAgent.chat(user_input)
         # logging.info(f"predicted answer is {predicted_answer}")
     except Exception as e:
         logging.error(f"Error during prediction: {e}")
